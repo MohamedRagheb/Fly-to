e2af773518af37132a19dc8d0f12134c5ff57394
Mohamed Ahmed Ragheb <98286948+MohamedRagheb@users.noreply.github.com>
2023-01-11 02:59:01 +0200

    Update indexmachine.js

--- js-files/indexmachine.js
+++ js-files/indexmachine.js
@@ -4,4 +4,13 @@ var pass = document.getElementById('pass')
 var link = document.getElementById('link')
 var alart = document.getElementById('alart')
 
-    
+    function ggd(){
+ user.defaultvalue = "admin"
+ pass.defaultvalue = "admin"
+    if(user.defaultvalue == user.value && pass.defaultvalue == pass.value){
+        window.location.href = "../main files/html-files/mainpage.html"
+    }else{
+        alart.innerHTML = "Wrong username or Password"
+    }
+}
+

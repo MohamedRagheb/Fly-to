c1ae77fcd453e2ac13636a3410ae0211a4e94326
Mohamed Ahmed Ragheb <98286948+MohamedRagheb@users.noreply.github.com>
2023-01-11 02:31:45 +0200

    No password and Username

--- js-files/indexmachine.js
+++ js-files/indexmachine.js
@@ -3,14 +3,5 @@ var user = document.getElementById('user')
 var pass = document.getElementById('pass')
 var link = document.getElementById('link')
 var alart = document.getElementById('alart')
-function ggd(){
- user.defaultvalue = "moody"
- pass.defaultvalue = "123456"
-    if(user.defaultvalue == user.value && pass.defaultvalue == pass.value){
-        window.location.href = "html-files/mainpage.html"
-    }else{
-        alart.innerHTML = "Wrong username or Password"
-    }
-}
 
     
